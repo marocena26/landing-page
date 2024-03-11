6a12e00813b83c097e8c1e53a7eaf973e8ba8c35
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-11 12:36:32 +0100

    feat(advantages): add provisional structure without styles for the advantages page

---
+++ src/app/interfaces/advantages.ts
@@ -0,0 +1,6 @@
+export interface Advantages {
+
+  name: string;
+  description: string;
+
+}
--- src/app/modules/pages/reasons-to-choose/reasons-to-choose.component.ts
+++ src/app/modules/pages/reasons-to-choose/reasons-to-choose.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Advantages } from 'src/app/interfaces/advantages';
 
 @Component({
   selector: 'reasons-to-choose',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./reasons-to-choose.component.scss']
 })
 export class ReasonsToChooseComponent {
+
+  advantages: Advantages[] = [];
+
+
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe(() => {
+      this.loadAdvantages();
+    });
+
+    this.loadAdvantages();
+  }
+
+  private loadAdvantages() {
+    this.advantages = [
+      {
+        name: this.translate.instant('advantageSubTitle'),
+        description: this.translate.instant('advantageDescription'),
+      },
+      {
+        name: this.translate.instant('advantageSubTitle2'),
+        description: this.translate.instant('advantageDescription2'),
+      },
+      {
+        name: this.translate.instant('advantageSubTitle3'),
+        description: this.translate.instant('advantageDescription3'),
+      },
+    ];
+  }
 
 }
